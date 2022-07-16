5a8b7c46f33c478b8cd36d39062742714a0e1e9b
Luis Monzon <60436830+Luis4609@users.noreply.github.com>
2022-07-16 10:38:12 +0200

    Layout page add

---
+++ pages/layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PersistentDrawerLeft from '../components/LeftDrawer'
+
+function layout() {
+  return (
+    <>
+    <PersistentDrawerLeft></PersistentDrawerLeft>
+    </>
+  )
+}
+
+export default layout
