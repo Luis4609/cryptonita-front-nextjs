79945d328d116824f8e358580ba40e82caf72dc7
Luis Monzon <60436830+Luis4609@users.noreply.github.com>
2022-07-22 12:53:24 +0200

    Added difference between previous value of the Bitcoin

--- pages/socket.jsx
+++ pages/socket.jsx
@@ -1,25 +1,29 @@
+import { style } from "@mui/system";
 import React, { useState, useCallback, useEffect } from "react";
 import useWebSocket, { ReadyState } from "react-use-websocket";
+import styles from "../styles/Home.module.css";
 
 const WebSocketDemo = () => {
   //Public API that will echo messages sent to it back to the client
-  const [socketUrl, setSocketUrl] = useState("wss://echo.websocket.org");
+  const [socketUrl, setSocketUrl] = useState(
+    "wss://ws.coincap.io/prices?assets=bitcoin"
+  );
   const [messageHistory, setMessageHistory] = useState([]);
 
   const { sendMessage, lastMessage, readyState } = useWebSocket(socketUrl);
 
-  // useEffect(() => {
-  //   if (lastMessage !== null) {
-  //     setMessageHistory((prev) => prev.concat(lastMessage));
-  //   }
-  // }, [lastMessage, setMessageHistory]);
+  useEffect(() => {
+    if (lastMessage !== null) {
+      setMessageHistory((prev) => prev.concat(lastMessage));
+    }
+  }, [lastMessage, setMessageHistory]);
 
-  const handleClickChangeSocketUrl = useCallback(
-    () => setSocketUrl("wss://ws.coincap.io/prices?assets=bitcoin"),
-    []
-  );
+  // const handleClickChangeSocketUrl = useCallback(
+  //   () => setSocketUrl("wss://ws.coincap.io/prices?assets=bitcoin"),
+  //   []
+  // );
 
-  const handleClickSendMessage = useCallback(() => sendMessage("Hello"), []);
+  // const handleClickSendMessage = useCallback(() => sendMessage("Hello"), []);
 
   const connectionStatus = {
     [ReadyState.CONNECTING]: "Connecting",
@@ -29,9 +33,16 @@ const WebSocketDemo = () => {
     [ReadyState.UNINSTANTIATED]: "Uninstantiated",
   }[readyState];
 
+  function isCoinEqual(object1, object2) {
+    console.log(`%cCheck mayor que: ${object1 >= object2}`, "color: red ;");
+    console.log(`%cCheck valor que: ${object1}`, "color: green ;");
+
+    return object1 >= object2;
+  }
+
   return (
     <div>
-      <button onClick={handleClickChangeSocketUrl}>
+      {/* <button onClick={handleClickChangeSocketUrl}>
         Click Me to change Socket Url
       </button>
       <button
@@ -39,17 +50,26 @@ const WebSocketDemo = () => {
         disabled={readyState !== ReadyState.OPEN}
       >
         Click Me to send 'Hello'
-      </button>
+      </button> */}
       <span>The WebSocket is currently {connectionStatus}</span>
       {lastMessage ? <span>Last message: {lastMessage.data}</span> : null}
       <ul>
-        {/* {messageHistory.map((message, idx) => (
-          <span key={idx}>{message ? message.data : null}</span>
-        ))} */}
-
+        {messageHistory.map((message, idx, subida) => (
+          <span
+            key={idx}
+            className={
+              isCoinEqual(subida[idx].data, subida[idx - 1]?.data)
+                ? styles.verde
+                : styles.rojo
+            }
+          >
+            {message ? message.data : null}
+            {/* {console.log(subida[idx].data)} */}
+          </span>
+        ))}
       </ul>
     </div>
   );
 };
 
-export default WebSocketDemo
+export default WebSocketDemo;
