abd9712774b8bfbd137e1bb9f38508ae78de4a86
Luis Monzon <60436830+Luis4609@users.noreply.github.com>
2022-07-23 14:19:47 +0200

    Page for coin with dynamic path

--- pages/coins/[cid].tsx
+++ pages/coins/[cid].tsx
@@ -1,39 +1,80 @@
-import { useState } from "react";
+// import { NextPage } from "next";
+// import { useRouter } from "next/router";
+// import { Coin } from "../../types/coin";
+// import { Typography } from "@mui/material";
+// import { useEffect, useState } from "react";
+// import styles from "../../styles/Home.module.css";
+
+// const API_URL: string = "https://api.coincap.io/v2/assets/";
+
+// const CoinData: NextPage = () => {
+//   const router = useRouter();
+//   const { cid } = router.query;
+
+//   const [coin, setCoin] = useState();
+
+//   useEffect(() => {
+//     fetch(API_URL + cid)
+//       .then((res) => res.json())
+//       .then((data) => setCoin(data));
+//   }, []);
+
+//   if (!coin) return <h1>Loading...</h1>;
+
+//   return (
+//     <main className={styles.main}>
+//       <h1 className={styles.title}>Historic Data</h1>
+//       {/* <Chart></Chart> */}
+//       <Typography>{coin}</Typography>
+//       {/* <Typography>{coin.rank}</Typography>
+
+//       <Typography>{coin.marketCapUsd}</Typography> */}
+//     </main>
+//   );
+// };
+
+// export async function getStaticProps() {
+//   const res = await fetch(API_URL + "bitcoin");
+//   const data: Coin = await res.json();
+
+//   return {
+//     props: { coin: data }, // will be passed to the page component as props
+//   };
+// }
+
+// export async function getStaticPaths() {
+//   return {
+//     paths: Array<string | { params: { [key: string]: string } }>,
+//     fallback: true
+//   }
+// }
+
+// export default CoinData;
+
 import { useRouter } from "next/router";
-import { InferGetStaticPropsType } from "next/types";
-import { Coin } from "../../types/coin";
-import { Typography } from "@mui/material";
-import styles from "../../styles/Home.module.css";
+import { useState, useEffect } from "react";
+import { Typography, Container } from "@mui/material";
 
-const API_URL: string = "https://api.coincap.io/v2/assets/";
-
-const CoinData = ({ data }: InferGetStaticPropsType<typeof getStaticProps>) => {
+const CoinData = () => {
   const router = useRouter();
   const { cid } = router.query;
 
-  const [coin, setCoin] = useState(data);
+  const [coin, setCoin] = useState();
 
-  if (!coin) return <h1>Loading...</h1>;
+  useEffect(() => {
+    fetch("https://api.coincap.io/v2/assets/" + cid)
+      .then((res) => res.json())
+      .then((data) => setCoin(data.data));
+  }, []);
 
   return (
-    <main className={styles.main}>
-      <h1 className={styles.title}>Historic Data</h1>
-      {/* <Chart></Chart> */}
-      <Typography>{coin.data.id}</Typography>
-      <Typography>{coin.data.rank}</Typography>
-
-      <Typography>{coin.data.marketCapUsd}</Typography>
-    </main>
+    <Container>
+      <Typography variant="h2" color="secondary">
+        Título: {coin?.rank}
+      </Typography>
+      <p>Post: {cid}</p>
+    </Container>
   );
 };
-
-export async function getStaticProps() {
-  const res = await fetch(API_URL + "bitcoin");
-  const data: Coin = await res.json();
-
-  return {
-    props: { data }, // will be passed to the page component as props
-  };
-}
 
 export default CoinData;
