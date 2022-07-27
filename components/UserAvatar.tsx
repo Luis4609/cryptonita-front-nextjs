ba0559bbd6c8c303d58a2a5f07c14087aceec497
Luis4609 <luis.monzon.pedreira@alumnos.upm.es>
2022-07-27 17:48:22 +0200

    UserAvatar component

---
+++ components/UserAvatar.tsx
@@ -0,0 +1,47 @@
+import Avatar from "@mui/material/Avatar";
+import { deepOrange } from "@mui/material/colors";
+import { styled } from "@mui/material/styles";
+import Badge from "@mui/material/Badge";
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  "& .MuiBadge-badge": {
+    backgroundColor: "#44b700",
+    color: "#44b700",
+    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+    "&::after": {
+      position: "absolute",
+      top: 0,
+      left: 0,
+      width: "100%",
+      height: "100%",
+      borderRadius: "50%",
+      animation: "ripple 1.2s infinite ease-in-out",
+      border: "1px solid currentColor",
+      content: '""',
+    },
+  },
+  "@keyframes ripple": {
+    "0%": {
+      transform: "scale(.8)",
+      opacity: 1,
+    },
+    "100%": {
+      transform: "scale(2.4)",
+      opacity: 0,
+    },
+  },
+}));
+
+function UserAvatar() {
+  return (
+    <StyledBadge
+      overlap="circular"
+      anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+      variant="dot"
+    >
+      <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+    </StyledBadge>
+  );
+}
+
+export default UserAvatar;
